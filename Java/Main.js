8365efaf188aa4bf4ecfadd392022cc5959d4d4e
Andrew Murray <darbyguitars@gmail.com>
2019-11-04 11:58:44 -0600

    Messed up my push, re ship orientation/user input functions

--- Java/Main.js
+++ Java/Main.js
@@ -2,6 +2,9 @@
 let distanceTraveled = 0
 let direction = 'NE'   // NW , SW , SE
 let shipOrientation = 'E'
+let xSpeed = 0
+let ySpeed = 0 
+
 //                                                                       Main Menu
 
 
@@ -19,12 +22,33 @@ let shipOrientation = 'E'
 let userInputSpeed = 2 //Placeholder
 let userInputAngle = 45 //Placeholder
 
+function reactToInput (){
+}
+let distanceTravY = 0  // Placeholder Reassign to input 
+let distanceTravX = 0  // and if statements
+if (userInputAngle > 360) {
+    return
+}
+else if (userInputAngle > 270){  // ◣
+    userInputAngle = userInputAngle - 270
+    distanceTravX = Math.sin(userInputAngle) * userInputSpeed
+    distanceTravY = Math.sign(Math.cos(userInputAngle) * userInputSpeed)
+}
+else if (userInputAngle > 180){  // ◤
+    userInputAngle = userInputAngle - 180
+    distanceTravX = Math.sign(Math.sin(userInputAngle) * userInputSpeed)
+    distanceTravY = Math.sign(Math.cos(userInputAngle) * userInputSpeed)
 
-
-
-Math.sin(userInputAngle) * userInputSpeed
-
-
+}
+else if (userInputAngle > 90){  //  ◥
+    userInputAngle = userInputAngle - 90
+    distanceTravY = Math.sin(userInputAngle) * userInputSpeed
+    distanceTravX = Math.cos(userInputAngle) * userInputSpeed
+}
+else if (userInputAngle >= 0){    //  ◢                                                      
+    distanceTravY = Math.sin(userInputAngle) * userInputSpeed
+    distanceTravX = Math.cos(userInputAngle) * userInputSpeed
+}
 
 
 
