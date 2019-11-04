1a1e1cf940965c09fafee247c81ad5513250b639
Andrew Murray <darbyguitars@gmail.com>
2019-11-04 15:18:25 -0600

    Created the code that the user interface interacts with

--- Java/Main.js
+++ Java/Main.js
@@ -1,50 +1,57 @@
-//                                                                       Global Variables
+/**
+ * Global Variables
+ *  */                                                                 
 let distanceTraveled = 0
 let xSpeed = 0
 let ySpeed = 0 
 let shipOrientation = 0
-
-//                                                                       Main Menu
-
-
-
-
-
-//                                                                      User Input for Move
+let currentSpeed = 0
+/** 
+ * Main Menu
+ * 
+ * */                                                                       
 
 
 
+/**
+ *  User Input a Move Menu!
+ * */                                                                      
 
-//                                                                      Moving / Key Function
+
+
+/**
+ * Movement and orientation Functions
+ * @function movementAndRotation : This function calculates the ships current speed and direction by taking the users inputs of Angle and Speed and contrasting themm against eachother 
+ *  */                                                                      
 
 
 let userInputSpeed = 2 //Placeholder
 let userInputAngle = 45 //Placeholder
 
-function reactToInput (){
+function turnStart (){
 
 let distanceTravY = 0  // Placeholder Reassign to input 
 let distanceTravX = 0  // and if statements
 if (userInputAngle > 360) {
     return
 }
-else if (userInputAngle > 270){  // ◣
+else if (userInputAngle > 270){  // ◣ x , -y
     userInputAngle = userInputAngle - 270
     distanceTravX = Math.sin(userInputAngle) * userInputSpeed
-    distanceTravY = Math.sign(Math.cos(userInputAngle) * userInputSpeed)
+    distanceTravY = Math.cos(userInputAngle) * userInputSpeed * -1
 }
-else if (userInputAngle > 180){  // ◤
+else if (userInputAngle > 180){  // ◤ -x , -y
     userInputAngle = userInputAngle - 180
-    distanceTravX = Math.sign(Math.sin(userInputAngle) * userInputSpeed)
-    distanceTravY = Math.sign(Math.cos(userInputAngle) * userInputSpeed)
+    distanceTravX = Math.sin(userInputAngle) * userInputSpeed * -1
+    distanceTravY = Math.cos(userInputAngle) * userInputSpeed * -1
 
 }
-else if (userInputAngle > 90){  //  ◥
+else if (userInputAngle > 90){  // ◥ -x , y
     userInputAngle = userInputAngle - 90
-    distanceTravX = Math.sign(Math.sin(userInputAngle) * userInputSpeed)
+    distanceTravX = Math.sin(userInputAngle) * userInputSpeed * -1
     distanceTravY = Math.cos(userInputAngle) * userInputSpeed
 }                                                  
-else if (userInputAngle >= 0){    //  ◢   
+else if (userInputAngle >= 0){  // ◢ x , y
     distanceTravY = Math.sin(userInputAngle) * userInputSpeed
     distanceTravX = Math.cos(userInputAngle) * userInputSpeed
 }
@@ -52,10 +59,26 @@ else if (userInputAngle >= 0){    //  ◢
 
 }
 
-function movementActual(){
-    let xSpeed = distanceTravX + xSpeed
-    let ySpeed = distanceTravY + ySpeed
+function movementAndRotation(){
+    xSpeed = distanceTravX + xSpeed
+//Accumulates speed along the x axis
+    ySpeed = distanceTravY + ySpeed
+//Accumulates speed along the y axis
+    currentSpeed = Math.sqrt((xSpeed * xSpeed) + (ySpeed * ySpeed))
+    distanceTraveled = distanceTraveled + currentSpeed
+    //Helps determine the distance for scoring
+    shipOrientation = Math.atan(Math.abs(xSpeed) / Math.abs(ySpeed)) * (180 / Math.PI)
+if (xSpeed >= 0){ //positive x
+    ySpeed >= 0 ? 0 : shipOrientation = shipOrientation + 270 ;
+    }
+else if (xSpeed < 0){ //negative x
+    ySpeed >= 0 ? shipOrientation = shipOrientation + 90 : shipOrientation = shipOrientation + 180;
+    }
 
 }
 
-//                                                                      Highscore Shtuff
+/**
+ *  Highscore Stuff
+ * 
+ * 
+ *  */                                                                     
