206b4270550f03d33176ea63b2eeac743737445a
Andrew Murray <darbyguitars@gmail.com>
2019-11-04 11:56:35 -0600

    Setting up the ship orientation as well as the user inputted distance

---
+++ Java/Main.js
@@ -0,0 +1,32 @@
+//                                                                       Global Variables
+let distanceTraveled = 0
+let direction = 'NE'   // NW , SW , SE
+let shipOrientation = 'E'
+//                                                                       Main Menu
+
+
+
+
+
+//                                                                      User Input for Move
+
+
+
+
+//                                                                      Moving / Key Function
+
+
+let userInputSpeed = 2 //Placeholder
+let userInputAngle = 45 //Placeholder
+
+
+
+
+Math.sin(userInputAngle) * userInputSpeed
+
+
+
+
+
+
+//                                                                      Highschore Shtuff
