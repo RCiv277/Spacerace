7cc141fe6622fc77ebdfb246a3d7352b139dfaeb
Andrew Murray <darbyguitars@gmail.com>
2019-11-04 12:45:23 -0600

    More structuring, distance is configured, Working on computing ship orientation

--- Java/Main.js
+++ Java/Main.js
@@ -1,9 +1,8 @@
 //                                                                       Global Variables
 let distanceTraveled = 0
-let direction = 'NE'   // NW , SW , SE
-let shipOrientation = 'E'
 let xSpeed = 0
 let ySpeed = 0 
+let shipOrientation = 0
 
 //                                                                       Main Menu
 
@@ -23,7 +22,7 @@ let userInputSpeed = 2 //Placeholder
 let userInputAngle = 45 //Placeholder
 
 function reactToInput (){
-}
+
 let distanceTravY = 0  // Placeholder Reassign to input 
 let distanceTravX = 0  // and if statements
 if (userInputAngle > 360) {
@@ -42,15 +41,21 @@ else if (userInputAngle > 180){  // ◤
 }
 else if (userInputAngle > 90){  //  ◥
     userInputAngle = userInputAngle - 90
+    distanceTravX = Math.sign(Math.sin(userInputAngle) * userInputSpeed)
+    distanceTravY = Math.cos(userInputAngle) * userInputSpeed
+}                                                  
+else if (userInputAngle >= 0){    //  ◢   
     distanceTravY = Math.sin(userInputAngle) * userInputSpeed
     distanceTravX = Math.cos(userInputAngle) * userInputSpeed
 }
-else if (userInputAngle >= 0){    //  ◢                                                      
-    distanceTravY = Math.sin(userInputAngle) * userInputSpeed
-    distanceTravX = Math.cos(userInputAngle) * userInputSpeed
+
+
 }
 
+function movementActual(){
+    let xSpeed = distanceTravX + xSpeed
+    let ySpeed = distanceTravY + ySpeed
 
+}
 
-
-//                                                                      Highschore Shtuff
+//                                                                      Highscore Shtuff
